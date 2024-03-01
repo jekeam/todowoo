07cdc0eec7a36648ac4d1b37065fb2139c2141f6
ptyhondev <ptyhondev@gmail.com>
2024-03-01 21:11:12 +0300

    second commit

---
+++ todo/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Todo
+
+class TodoAdmnin(admin.ModelAdmin):
+    readonly_fields = ('created', )
+
+
+
+
+admin.site.register(Todo, TodoAdmnin)
+
+
+
